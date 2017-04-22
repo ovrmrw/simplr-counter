ee16ab8440b52e7a5df887ddd8111f8791ab395a
ovrmrw <t.ngc.0403@gmail.com>
2017-04-22 23:32:39 +0900

    Update

--- src/app/services/counter.ts
+++ src/app/services/counter.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Dispatcher } from '@ngrx/store';
 import { Simplr } from 'ngrx-store-simplr';
+
 import { AppState } from '../store/models';
 import * as counter from '../store/actions/counter';
+import { initialState } from '../store/reducer';
 
 
 @Injectable()
@@ -12,14 +14,15 @@ export class CounterService {
     private dispatcher$: Dispatcher,
   ) { }
 
-  async increment(num: number) {
-    const result = await this.simplr
-      .dispatch('counter', (state) => ({ value: state.value + num }))
-      .toPromise();
-    return result.action;
+  increment(num: number) {
+    this.simplr.dispatch('counter', (state) => ({ value: state.value + num }));
   }
 
   reset() {
+    this.simplr.dispatch('counter', () => ({ ...initialState.counter }));
+  }
+
+  resetByNgrxStyle() {
     this.dispatcher$.dispatch(new counter.ActionReset());
   }
 }

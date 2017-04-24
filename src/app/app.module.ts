ba39af03aaea3f56c2531e39f3829dc62a420f79
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2017-04-24 14:25:01 +0900

    Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { Simplr, Adapter, AdapterForNgrxStore } from 'ngrx-store-simplr';
+import { Simplr, Adapter, AdapterForNgrxStore, SimplrModule } from 'ngrx-store-simplr';
 
 import { AppComponent } from './app.component';
 import { CounterContainerComponent } from './containers/counter';
@@ -25,10 +25,9 @@ import { reducer, initialState } from './store/reducer';
     HttpModule,
     StoreModule.provideStore(reducer, initialState),
     StoreDevtoolsModule.instrumentStore(),
+    SimplrModule.forRoot(),
   ],
   providers: [
-    Simplr,
-    { provide: Adapter, useClass: AdapterForNgrxStore },
     CounterService,
     CounterNgrxService,
   ],

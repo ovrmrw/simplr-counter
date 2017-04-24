f551608f24476eb16d88e021d7999be168d00aeb
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2017-04-24 10:01:06 +0900

    Update

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -7,13 +7,24 @@ import { counterNgrxReducer } from './reducers/counter-ngrx';
 const wrapper = new Wrapper<AppState>();
 
 
-const finalReducer = combineReducers({
-  counter: wrapper.createWrappedReducer('counter'),
+// const finalReducer = combineReducers({
+//   counter: wrapper.createWrappedReducer('counter'),
+//   'counter-ngrx': counterNgrxReducer,
+// });
+
+// export function reducer(state, action) {
+//   return finalReducer(state, action);
+// }
+
+const wrappedReducers = wrapper.mergeReducersIntoWrappedReducers({
+  counter: null,
   'counter-ngrx': counterNgrxReducer,
 });
 
+const rootReducer = combineReducers(wrappedReducers);
+
 export function reducer(state, action) {
-  return finalReducer(state, action);
+  return rootReducer(state, action);
 }
 
 export const initialState: AppState = {
